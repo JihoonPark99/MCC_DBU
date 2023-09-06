d97a251fd812130d3a2d85d82039897700ccf656
jihoon <jihoonmanse@gmail.com>
2023-09-06 15:28:03 -0500

    JihoonParkHello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          feature-aws 1
+          Jihoon Park, Hello 
         </p>
         <a
           className="App-link"
